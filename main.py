2b3ae5d5227313d7ad9964a3fc2e857fe58f0029
Eric Berger <eric@brrgrr.com>
2017-09-27 15:53:02 -0500

    Bootstrap refactoring

--- main.py
+++ main.py
@@ -5,13 +5,25 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
+title = 'User Signup'
+
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template(
+        'index.html',
+        title=title
+    )
+
 
 @app.route("/welcome")
 def welcome():
-    return render_template('welcome.html',username="Bob" )
+    return render_template(
+        'welcome.html',
+        title=title,
+        username="Bob"
+    )
+
 
 if __name__ == '__main__':
     app.run()
